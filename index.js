29c595b383c58883d7cffcebbe430edf811931d6
Teyanna Earle <teyonna.earle92@gmail.com>
2021-11-11 11:56:57 -0500

    add lab answers

--- index.js
+++ index.js
@@ -10,7 +10,37 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    if (this.root.data === data) return this.root;
+
+    let current = [...this.root.children];
+    while (current.length) {
+      let point = current.shift();
+
+      if (point.data === data) return point;
+      if (point.children) {
+        current.push(...point.children);
+      }
+    }
+
+    return null;
+  }
 }
+
+const a = new TreeNode("A");
+const b = new TreeNode("B");
+const c = new TreeNode("C");
+const d = new TreeNode("D");
+const e = new TreeNode("E");
+const f = new TreeNode("F");
+const g = new TreeNode("G");
+const h = new TreeNode("H");
+a.children.push(b, c, d);
+b.children.push(e, f, g);
+d.children.push(h);
+
+let treeA = new Tree(a);
+// console.log(treeA.findNode());
+// treeA.findNode();
 
 module.exports = { TreeNode, Tree };
