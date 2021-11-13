bd7909c7f5869548c1967b4e11ad4eeb4012a92b
Du Min Chen <dm.chen0113@gmail.com>
2021-11-13 11:39:40 -0500

    Trees introduction lab finished

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    if (!this.root)
+      return null;
+
+    const queue = [this.root];
+    while (queue.length) {
+      const node = queue.shift();
+      if (node.data === data)
+        return node;
+
+      if (node.children.length) {
+        queue.push(...node.children);
+      }
+    }
+    return null;
+  }
 }
 
 module.exports = { TreeNode, Tree };
