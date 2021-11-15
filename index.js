87fd0aa5c45442b506b546a75f7a7e745056b8a1
Yesenia L <ylopezcr123@gmail.com>
2021-11-14 23:57:27 -0500

    trees

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    if (!this.root)
+      return null;
+
+    const next = [this.root];
+    while (next.length) {
+      const node = next.shift();
+      if (node.data === data)
+        return node;
+
+      if (node.children.length) {
+        next.push(...node.children);
+      }
+    }
+    return null;
+  }
 }
 
 module.exports = { TreeNode, Tree };
