23ed55f74db1d37a7ef919895a646b020aec42cb
Angelina Ebreo <angelinaebreo@gmail.com>
2021-11-15 13:19:23 -0500

    complete tree lab

--- index.js
+++ index.js
@@ -5,22 +5,14 @@ class TreeNode {
   }
 
   hasVal(val) {
-    console.log("current node", this.data, this.data === val);
-    console.log(this);
-    // const valueArr = [];
-    // const hasValue = this.data === val
     if (this.data === val) return this;
-
     for (let child of this.children) {
-      console.log("current child", child.data);
-
-      child.hasVal(val);
+      const result = child.hasVal(val);
+      if (result) {
+        return result;
+      }
     }
-
     return null;
-
-    //const result = valueArr.filter((el) => el);
-    //return this.data === val ? this : null
   }
 }
 
@@ -30,52 +22,8 @@ class Tree {
   }
 
   findNode(data) {
-    // let curNode = this.root
-    // while (curNode) {
-    //   if (data < curNode.data) {
-    //     curNode = curNode.left
-    //   } else if (data > curNode.data) {
-    //     curNode = curNode.right
-    //   } else {
-    //     return curNode
-    //   }
-    // }
-    // return null
-
     return this.root.hasVal(data);
   }
 }
 
 module.exports = { TreeNode, Tree };
-
-let treeB;
-
-/*
-    ┌─A─┐
-  ┌─B   C─┐
-  D       E─┐ 
-            F─┐
-              G─┐
-                H─┐
-                  I     
-*/
-const a = new TreeNode("A");
-const b = new TreeNode("B");
-const c = new TreeNode("C");
-const d = new TreeNode("D");
-const e = new TreeNode("E");
-const f = new TreeNode("F");
-const g = new TreeNode("G");
-const h = new TreeNode("H");
-const i = new TreeNode("I");
-a.children.push(b, c);
-b.children.push(d);
-c.children.push(e);
-e.children.push(f);
-f.children.push(g);
-g.children.push(h);
-h.children.push(i);
-treeB = new Tree(a);
-
-console.log(treeB.findNode("I"));
-//console.log(h.hasVal("H"))
