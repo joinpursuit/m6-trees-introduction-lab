f2ff4d30258fcc4356e0c7f8cd72558620c97e8a
Jamee Hengari <jhengari@hotmail.com>
2021-11-15 21:19:36 -0500

    trees lab practice

--- index.js
+++ index.js
@@ -10,7 +10,26 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    let root = this.root;
+    if (data === root.data) {
+      return root;
+    }
+    // while (root.children.length > 0) {
+    //   let children = [];
+    //   for (let i = 0; i < root.children.length; i++) {
+    //     if (root.children[i].data === data) {
+    //       root = root.children[i];
+    //       children = null;
+    //       break;
+    //     } else {
+    //       children.push(root.children[i]);
+    //     }
+    //   }
+    //   root.children = children;
+    // }
+    return root;
+  }
 }
 
 module.exports = { TreeNode, Tree };
