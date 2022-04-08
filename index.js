92ddd841cb1e52b19101d418c6b1a5702b0e780f
DylanLovell <arcanyne7@gmail.com>
2022-04-08 14:21:16 -0400

    findNode

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    const searchNode = (currentNode = this.root) =>
+      currentNode.data === data
+        ? currentNode
+        : currentNode.children
+            .map((cNode) => searchNode(cNode))
+            .find((e) => !e === false) || null;
+    return searchNode();
+  }
 }
 
 module.exports = { TreeNode, Tree };
