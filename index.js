5cd6b24d3a7e03f766cab6f5863ac02b4ee36bdd
Billy Dominique <billydominique@pursuit.org>
2022-04-08 23:03:41 -0400

    passed the test

--- index.js
+++ index.js
@@ -10,7 +10,54 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+  
+    if(this.root.data === data) return this.root
+    
+    for (let child of this.root.children){
+      
+      let subTree = new Tree(child)
+      let foundNode = subTree.findNode(data)
+      
+      if (foundNode) return foundNode
+      
+    }
+    return null
+  }
 }
 
 module.exports = { TreeNode, Tree };
+
+// findNode(data) {
+  
+//   console.log("starting function for",`${this.root.data}.findNode(${data})`)
+//   if( this.root.data === data){
+//     console.log("found data, ending function for",`${this.root.data}.findNode(${data})`)
+//     return(this.root)
+
+//   }
+//   // if(this.root.children.length){
+//     for (let child of this.root.children){
+//       // console.log(child)
+//       let subTree = new Tree(child)
+//       let result = subTree.findNode(data)
+//       if (result){
+//         console.log("result data, ending function for",`${this.root.data}.findNode(${data})`)
+//         return result
+//       } 
+//     }
+//   // } else {
+//     console.log("nothing found, ending function for",`${this.root.data}.findNode(${data})`)
+//     return null
+//   // } 
+
+
+  
+
+
+  
+
+// }
+
+
+// }
