61bcc174374361ca775b1e85d27c5271df04f873
kisublee <yigi2021@gmail.com>
2022-04-12 07:42:06 -0400

    Completed.

--- index.js
+++ index.js
@@ -10,7 +10,34 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+
+
+  findNode (data) {
+
+    let current = this.root
+
+          if (current === null) return null
+
+          if (current.data === data) {
+            return current
+          }  else {
+            if (current.children.length) {
+              for (const child of current.children) {
+                let newTree = new Tree(child)
+                let find = newTree.findNode(data)
+    
+                if (find) {
+                  return find
+                }
+              }
+            }
+          }
+
+          return null
 }
+ 
+
+}
+
 
 module.exports = { TreeNode, Tree };
