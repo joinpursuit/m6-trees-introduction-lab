0ea6e60a834390733d45fdee1c00faf59835ac49
Fatema Kabir <fatema.kabir@pursuit.org>
2022-04-13 12:26:40 -0400

    planted

--- index.js
+++ index.js
@@ -10,7 +10,36 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    let node = this.root;
+    if (data === node.data) {
+      return node;
+    } else {
+      for (let child of node.children) {
+        let newTree = new Tree(child);
+        let result = newTree.findNode(data);
+        if (result) {
+          return result;
+        }
+      }
+    }
+    return null;
+  }
 }
+
+const a = new TreeNode("A");
+const b = new TreeNode("B");
+const c = new TreeNode("C");
+const d = new TreeNode("D");
+const e = new TreeNode("E");
+const f = new TreeNode("F");
+const g = new TreeNode("G");
+const h = new TreeNode("H");
+a.children.push(b, c, d);
+b.children.push(e, f, g);
+d.children.push(h);
+treeA = new Tree(a);
+
+console.log(treeA.findNode("B"));
 
 module.exports = { TreeNode, Tree };
