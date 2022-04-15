477961602ac3c948afbb8c33db656543b968755d
Angie G <angieg@Angies-MBP.fios-router.home>
2022-04-15 15:50:25 -0400

    Trees Lab

--- index.js
+++ index.js
@@ -10,11 +10,31 @@ class Tree {
     this.root = node;
   }
 
-  //return a reference to the root node if it exists
-  findNode(data, node = this.root) {
-    if (node.data === data) {
-      return node;
+  //Return a reference to the root node if it exists
+  findNode(data) {
+    //accessing root data and children
+    let rootData = this.root.data;
+    let rootChild = this.root.children;
+
+    //if the root data is equal to the data given to us
+    if (rootData === data) {
+      //then return the node (root)
+      return this.root;
     }
+
+    for (let i = 0; i < rootChild.length; i++) {
+      //creating the new Tree
+      let newTree = new Tree(rootChild[i]);
+      //found is the newTree with the inputted data
+      let found = newTree.findNode(data);
+      //if the newTree with inputted data is true
+      if (found) {
+        //then return it
+        return found;
+      }
+    }
+    //otherwise return null
+    return null;
   }
 }
 
