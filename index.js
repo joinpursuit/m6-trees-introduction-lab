f3d19f077c7b7038a681368a2dc6e01b3cc3a6d1
Lortiz528 <lortiztech@gmail.com>
2022-10-20 12:57:06 -0400

    new way using newtree class

--- index.js
+++ index.js
@@ -9,30 +9,45 @@ class Tree {
   constructor(node) {
     this.root = node;
   }
-
-  findNode(data, currentNode = this.root) {
-    if (currentNode.data === data) {
-      return currentNode;
-    } else if (currentNode.children) {
-      return (
-        currentNode.children
-          .map((child) => {
-            return this.findNode(data, child);
-          })
-          .find((child) => {
-            if (child !== null) {
-              return child;
-            }
-          }) || null
-      );
+  findNode(data) {
+    if (this.root.data === data) {
+      return this.root;
     }
 
-    return currentNode.children.map((child) => {
-      if (child.data === data) {
-        return child;
+    for (let child of this.root.children) {
+      let newTreeChild = new Tree(child);
+      // console.log(newTreeChild);
+      let res = newTreeChild.findNode(data);
+      if (res) {
+        return res;
       }
-    });
+    }
+    return null;
   }
+  // group work solution
+  // findNode(data, currentNode = this.root) {
+  //   if (currentNode.data === data) {
+  //     return currentNode;
+  //   } else if (currentNode.children) {
+  //     return (
+  //       currentNode.children
+  //         .map((child) => {
+  //           return this.findNode(data, child);
+  //         })
+  //         .find((child) => {
+  //           if (child !== null) {
+  //             return child;
+  //           }
+  //         }) || null
+  //     );
+  //   }
+
+  //   return currentNode.children.map((child) => {
+  //     if (child.data === data) {
+  //       return child;
+  //     }
+  //   });
+  // }
 }
 
 module.exports = { TreeNode, Tree };
