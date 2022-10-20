175fc86c9d7bb96e8468c1a97aa1413858bbd8cf
lchen6283 <lchen6283@gmail.com>
2022-10-20 14:13:49 -0400

    tree lab

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    let queue = [this.root]; //create queue
+    while (queue.length > 0) {
+      //loop if not empty
+      let node = queue.shift(); //extract out first node in the array
+      if (node.data === data) {
+        //if node equals data return node
+        return node;
+      }
+      for (let child of node.children) {
+        //loop push all children into queue
+        queue.push(child);
+      }
+    }
+    return null; //return null if node does not exist
+  }
 }
 
 module.exports = { TreeNode, Tree };
