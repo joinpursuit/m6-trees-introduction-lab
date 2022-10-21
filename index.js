47a59d18672600e32442804a67734d19303b34dd
Mason Mei <masonmei@pursuit.org>
2022-10-21 15:38:58 -0500

    restructured code

--- index.js
+++ index.js
@@ -10,31 +10,51 @@ class Tree {
     this.root = node;
   }
 
+  // findNode(data) {
+  //   let item = null;
+
+  //   this.traverseBFS((node) => {
+  //     if (node.data == data) {
+  //       item = node;
+  //     }
+  //   });
+  //   return item;
+  // }
+
+  // traverseBFS(cb) {
+  //   const queue = [this.root];
+
+  //   if (cb) {
+  //     while (queue.length) {
+  //       const node = queue.shift();
+
+  //       cb(node);
+
+  //       for (let child of node.children) {
+  //         queue.push(child);
+  //       }
+  //     }
+  //   }
+  // }
+
   findNode(data) {
-    let item = null;
-
-    this.traverseBFS((node) => {
-      if (node.data == data) {
-        item = node
+    let currentNode = this.root;
+    let queue = [this.root];
+    let item = null
+    
+    while (queue.length) {
+      currentNode = queue.shift();
+      if (currentNode.data === data) {
+        item = currentNode;
       }
-    })
-    return item
-  }
 
-  traverseBFS(cb) {
-    const queue = [this.root]
-
-    if (cb) {
-      while (queue.length) {
-        const node = queue.shift();
-
-        cb(node)
-
-        for (let child of node.children) {
-          queue.push(child)
-        }
+      if (currentNode.children.length > 0) {
+        for (let child of currentNode.children) {
+          queue.push(child);
+        } 
       }
     }
+    return item
   }
 }
 
