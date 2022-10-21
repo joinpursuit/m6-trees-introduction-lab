cd67326422e8c2bf8cbb7dde42e0f1924477b847
jede <jedebrahma@pursuit.org>
2022-10-21 16:47:43 -0400

    still not all clear

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ class Tree {
   constructor(node) {
     this.root = node;
   }
+  findNode(data) {
+    let currentNode = this.root;
+    let queue = [this.root];
+    let item = null
+    
+    while (queue.length) {
+      currentNode = queue.shift();
+      if (currentNode.data === data) {
+        item = currentNode;
+      }
 
-  findNode(data) {}
+      if (currentNode.children.length > 0) {
+        for (let child of currentNode.children) {
+          queue.push(child);
+        } 
+      }
+    }
+    return item
+  }
+
 }
 
 module.exports = { TreeNode, Tree };
