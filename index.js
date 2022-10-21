c096230d72913762c43b5029d896b6aaf511f8a1
TaryneLeach <taryneleach@gmail.com>
2022-10-20 20:23:38 -0400

    submitting

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 class TreeNode {
-  constructor(data = null) {
-    this.data = data;
-    this.children = [];
-  }
+	constructor(data = null) {
+		this.data = data;
+		this.children = [];
+	}
 }
 
 class Tree {
-  constructor(node) {
-    this.root = node;
-  }
+	constructor(node) {
+		this.root = node;
+	}
 
-  findNode(data) {}
+	findNode(data, theNode = [this.root]) {
+		while (theNode.length) {
+			let nodingOn = theNode.shift();
+			if (nodingOn.data.includes(data)) {
+				return nodingOn;
+			}
+
+			for (let child of nodingOn.children) {
+				// would anothr for loop work?
+				theNode.push(child);
+				// tried returning theNode but caused test to fail
+			}
+		}
+
+		return null;
+	}
 }
-
 module.exports = { TreeNode, Tree };
