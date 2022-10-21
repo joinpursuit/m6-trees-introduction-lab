08c5f98ec69272ca9e6ca14ea795938e2cd8e43c
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-10-20 23:27:20 -0400

    All tests passed after pair programming

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ class TreeNode {
     this.data = data;
     this.children = [];
   }
+  _findNode(data) {
+    if (this.data === data) {
+      return this;
+    }
+    let found = null;
+    for (let child of this.children) {
+      if (child._findNode(data)) {
+        found = child._findNode(data);
+        return found;
+      }
+    }
+    return found;
+  }
 }
 
 class Tree {
@@ -10,7 +23,9 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    return this.root._findNode(data);
+  }
 }
 
 module.exports = { TreeNode, Tree };
