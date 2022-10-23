607bffae83bf51aeae2526db46dcdcb37127ddca
Khaleed Uddin <uddin1786@gmail.com>
2022-10-23 11:18:47 -0400

    submission with Christine and Khaleed

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ class Tree {
   constructor(node) {
     this.root = node;
   }
+  // data = [A, D, H, Z, A, E, D, Z]
+  findNode(data, currentNode = this.root) {
+// console.log(currentNode.children)
 
-  findNode(data) {}
+    if (currentNode.data === data) {
+      return currentNode;
+    } else {
+      const array = currentNode.children.map((node) => this.findNode(data, node))
+      const foundNode = array.find((element) => element !== null) || null
+
+      return foundNode;
+    }
+  }
 }
+
+// let newTree = new Tree(new TreeNode("A"))
+// console.log(newTree.findNode("A"))
 
 module.exports = { TreeNode, Tree };
