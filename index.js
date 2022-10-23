098f07258d57fa03bdccb0dc7ea468963397eab8
Spencer Simon <spencer.san@gmail.com>
2022-10-23 11:42:41 -0400

    two solutions

--- index.js
+++ index.js
@@ -10,7 +10,39 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  // findNode(data) {
+  //   let queue = [this.root]; // create a queue with this.root in it
+  //   while (queue.length) {
+  //     // keep looping if queue is not empty
+  //     let node = queue.shift(); // shift out the first node in the queue/array
+  //     if (node.data === data) {
+  //       return node;
+  //     }
+  //     for (let child of node.children) {
+  //       queue.push(child);
+  //       // push each child of the current node into the queue
+  //     }
+  //   }
+  //   return null;
+  // }
+
+  findNode(data) {
+    if (!this.root) return null;
+    let item = null;
+    let queue = [this.root]; // create a queue with this.root in it
+    while (queue.length) {
+      // keep looping if queue is not empty
+      let item = queue.shift(); // shift out the first node in the queue/array
+      if (item.data === data) {
+        return item;
+      }
+      for (let child of item.children) {
+        queue.push(child);
+        // push each child of the current node into the queue
+      }
+    }
+    return item;
+  }
 }
 
 module.exports = { TreeNode, Tree };
