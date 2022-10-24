51d0b7b1a54d831b5f95b5145e87f118d27bf004
Edwin Codrington <edwincodrington@gmail.com>
2022-10-24 11:34:38 -0400

    lab solution

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 class TreeNode {
-  constructor(data = null) {
-    this.data = data;
-    this.children = [];
-  }
+	constructor(data = null) {
+		this.data = data;
+		this.children = [];
+	}
+	_getHeight() {}
 }
 
 class Tree {
-  constructor(node) {
-    this.root = node;
-  }
-
-  findNode(data) {}
+	constructor(node) {
+		this.root = node;
+	}
+	getHeight() {
+		return this.root._getHeight();
+	}
+	findNode(data) {
+		if (data === this.root.data) {
+			return this.root;
+		}
+		for (let child of this.root.children) {
+			let newRoot = new Tree(child);
+			let currentNode = newRoot.findNode(data);
+			if (currentNode !== null) {
+				return currentNode;
+			}
+		}
+		return null;
+	}
 }
 
 module.exports = { TreeNode, Tree };
