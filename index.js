4964b5537bf476786f5391062aab4ee827949671
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2024-01-23 00:28:10 -0500

    done

--- index.js
+++ index.js
@@ -6,11 +6,58 @@ class TreeNode {
 }
 
 class Tree {
-  constructor(node) {
+  constructor(node = null) {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    /*
+    console.log("data is " + data);
+    console.log("=============")
+    console.log(this.root)
+    console.log("=============")
+    */
+    if(!this.root)
+      return null;
+    
+    return this.postorder(this.root, data);
+  }
+
+  preorder(node, data){
+    if(!node){
+      return null;
+    }
+    if(node.data == data){
+      return node;
+    }
+    let temp;
+    
+    for(let item of node.children){
+      temp = this.preorder(item, data);
+      if(temp)
+        return temp;
+    }
+    return null;
+  }
+
+  postorder(node, data){
+    if(!node){
+      return null;
+    }
+    let temp;
+    
+    for(let item of node.children){
+      temp = this.postorder(item, data);
+      if(temp)
+        return temp;
+    }
+
+    if(node.data == data){
+      return node;
+    }
+
+    return null;
+  }
 }
 
 module.exports = { TreeNode, Tree };
