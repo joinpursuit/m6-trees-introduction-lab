ffbb1fe80f2b1f2a5487c2775621f795f35f0ee5
arim83 <jorammercado@pursuit.org>
2024-01-22 22:57:27 -0500

    lab complete

--- index.js
+++ index.js
@@ -10,7 +10,23 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    let result = null
+    function traverse(dataToFind, traverseNode) {
+      if (dataToFind === traverseNode.data) {
+        result = traverseNode
+        return
+      }
+      for (let i = 0; i < traverseNode.children.length; i++) {
+        if (result !== null)
+          break
+        if (traverseNode.children[i] !== undefined && traverseNode.children[i] !== null)
+          traverse(dataToFind, traverseNode.children[i])
+      }
+    }
+    traverse(data, this.root)
+    return result
+  }
 }
 
 module.exports = { TreeNode, Tree };
